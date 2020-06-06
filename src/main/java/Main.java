f51ec2012995f65db38f025f4db81e1310b8accd
Jakub <kubplon@gmail.com>
2020-06-07 00:54:58 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        //TODO: wstęp, potrzebne funkcje
+        /*
+         * Napiszę grę w wisielca.
+         * 1. Stwórz tablicę słów (10 elementów wystarczy)
+         * 2. Wylosuj słowo
+         * 3. Wyświetl pierwszy znak słowa oraz ostatni pozostałe pokaż jako "_"
+         * 4. Pod słowem wyświetl tyle "<3" ile pozostało wisielcowi (na początku niech to będzie 5), pamiętaj, że ilość serduszek musisz gdzieś przechowywać
+         * 5. Zapytaj gracza o literę.
+         *   a. Jeśli została znaleziona wyświetl 3 i 4 z literą w odpowiednim miejscu/miejscach
+         *   b. Jeśli nie została zmniejsz zmienną odpowiedzialną za ilość serduszek i wyświetl punkt 3 i 4
+         * 6. Jeśli graczowi udało się odgadnąć słowo i ma jeszcze serduszka wypisz: "Gratulacje!"
+         * 7. Jeśli graczowi skończyły się serduszka wypisz: "Przegrana :("
+         */
+    }
+}
